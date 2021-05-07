e1e08e5d0fe94734aedde9051dda4793c7c4dc57
Junghoon Kang <junghoon.jung.kang@gmail.com>
2021-05-07 09:50:14 +0900

    Update - model/scheduler.py - added sanity checks in main()

--- model/scheduler.py
+++ model/scheduler.py
@@ -1,4 +1,4 @@
-import torch.optim.lr_scheduler as scheduler
+import torch.optim.lr_scheduler
 from torch.optim.lr_scheduler import _LRScheduler
 
 
@@ -10,7 +10,7 @@ __all__ = [
 ]
 
 
-class StepLR(scheduler.StepLR):
+class StepLR(torch.optim.lr_scheduler.StepLR):
     """ Decays the learning rate of each parameter group by gamma every
     step_size epochs. Notice that such decay can happen simultaneously with
     other changes to the learning rate from outside this scheduler. When
@@ -22,12 +22,12 @@ class StepLR(scheduler.StepLR):
         Args:
             optimizer (torch.optim.Optimizer): Wrapped optimizer.
             step_size (int): Period of learning rate decay.
-            gamma (float): Multiplicative factor of learning rate decay. Default: 0.1.
-            last_epoch (int): The index of last epoch. Default: -1.
+            gamma (float, optional): Multiplicative factor of learning rate decay (default: 0.1).
+            last_epoch (int, optional): The index of last epoch (default: -1).
         """
         super(StepLR, self).__init__(optimizer, step_size, gamma, last_epoch)
 
-class MultiStepLR(scheduler.MultiStepLR):
+class MultiStepLR(torch.optim.lr_scheduler.MultiStepLR):
     """ Decays the learning rate of each parameter group by gamma once the
     number of epoch reaches one of the milestones. Notice that such decay
     can happen simultaneously with other changes to the learning rate from
@@ -39,12 +39,12 @@ class MultiStepLR(scheduler.MultiStepLR):
         Args:
             optimizer (torch.optim.Optimizer): Wrapped optimizer.
             milestones (list[int]): List of epoch indices. Must be increasing.
-            gamma (float): Multiplicative factor of learning rate decay. Default: 0.1.
-            last_epoch (int): The index of last epoch. Default: -1.
+            gamma (float, optional): Multiplicative factor of learning rate decay (default: 0.1).
+            last_epoch (int, optional): The index of last epoch (default: -1).
         """
         super(MultiStepLR, self).__init__(optimizer, milestones, gamma, last_epoch)
 
-class CosineAnnealingWarmRestarts(scheduler.CosineAnnealingWarmRestarts):  # FIXME: use lr_min, lr_max
+class CosineAnnealingWarmRestarts(torch.optim.lr_scheduler.CosineAnnealingWarmRestarts):  # FIXME: use lr_min, lr_max
     """ Set the learning rate of each parameter group using a cosine annealing
     schedule, where \eta_{max} is set to the initial lr, T_{cur} is the number
     of epochs since the last restart and T_{i} is the number of epochs between
@@ -62,9 +62,10 @@ class CosineAnnealingWarmRestarts(scheduler.CosineAnnealingWarmRestarts):  # FIX
         super(CosineAnnealingWarmRestarts, self).__init__(optimizer, T_0, T_multi, eta_min, last_epoch)
 
 class WarmupLR(_LRScheduler):
-    def __init__(self, optimizer, warmup_iterations, next_scheduler=None):
+    def __init__(self, optimizer, warmup_iterations, next_scheduler=None, debug=False):
         self.warmup_iterations = warmup_iterations
         self.next_scheduler = next_scheduler
+        self.debug = debug
         self.done = False
         super(WarmupLR, self).__init__(optimizer)
 
@@ -74,7 +75,8 @@ class WarmupLR(_LRScheduler):
                 if not self.done:
                     self.next_scheduler.base_lrs = self.base_lrs
                     self.done = True
-                    print('** WARMUP DONE **')
+                    if self.debug:
+                        print('** WARMUP DONE **')
                 return self.next_scheduler.get_last_lr()
             return self.base_lrs
         return [ base_lr * ( self.last_epoch / self.warmup_iterations ) for base_lr in self.base_lrs ]
@@ -96,25 +98,69 @@ if __name__ == '__main__':
     optimizer = SGD(model, lr=0.1)
     optimizer.zero_grad()
 
-    def StepLR_test():
-        scheduler = StepLR(optimizer, step_size=10, gamma=0.1)
+    def run(optimizer, scheduler):
         for epoch in range(40):
             print(epoch, optimizer.param_groups[0]['lr'])
             optimizer.step()
             scheduler.step()
+
+    def StepLR_test():
+        scheduler = StepLR(optimizer, step_size=10, gamma=0.1)
+        run(optimizer, scheduler)
     #StepLR_test()
 
     def MultiStepLR_test():
         scheduler = MultiStepLR(optimizer, milestones=[5,10,15], gamma=0.1)
-        for epoch in range(40):
-            print(epoch, optimizer.param_groups[0]['lr'])
-            optimizer.step()
-            scheduler.step()
+        run(optimizer, scheduler)
     #MultiStepLR_test()
 
-    #scheduler = scheduler_cosine = CosineAnnealingLR(optimizer, T_max=20)
-    #scheduler = scheduler_cosine_restart = CosineAnnealingWarmRestarts(optimizer, T_0=20)
-    #scheduler = scheduler_warm = WarmupLR(optimizer, warmup_iterations=5)
-    #scheduler = scheduler_warm_step = WarmupLR(optimizer, warmup_iterations=5, next_scheduler=scheduler_step)
-    #scheduler = scheduler_warm_cosine = WarmupLR(optimizer, warmup_iterations=5, next_scheduler=scheduler_cosine)
-    #scheduler = scheduler_warm_cosine_restart = WarmupLR(optimizer, warmup_iterations=5, next_scheduler=scheduler_cosine_restart)
+    def CosineAnnealingLR_test():
+        scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=20)
+        run(optimizer, scheduler)
+    #CosineAnnealingLR_test()
+
+    def CosineAnnealingWarmRestarts_test():
+        scheduler = CosineAnnealingWarmRestarts(optimizer, T_0=20)
+        run(optimizer, scheduler)
+    #CosineAnnealingWarmRestarts_test()
+
+    def WarmupLR_test():
+        def default_test():
+            scheduler = WarmupLR(
+                optimizer,
+                warmup_iterations = 5,
+                debug=True
+            )
+            run(optimizer, scheduler)
+        #default_test()
+
+        def StepLR_test():
+            scheduler = WarmupLR(
+                optimizer,
+                warmup_iterations = 5,
+                next_scheduler = StepLR(optimizer, step_size=10, gamma=0.1),
+                debug=True
+            )
+            run(optimizer, scheduler)
+        #StepLR_test()
+
+        def CosineAnnealingLR_test():
+            scheduler = WarmupLR(
+                optimizer,
+                warmup_iterations = 5,
+                next_scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=20),
+                debug=True
+            )
+            run(optimizer, scheduler)
+        #CosineAnnealingLR_test()
+
+        def CosineAnnealingWarmRestarts_test():
+            scheduler = WarmupLR(
+                optimizer,
+                warmup_iterations = 5,
+                next_scheduler = CosineAnnealingWarmRestarts(optimizer, T_0=20),
+                debug=True
+            )
+            run(optimizer, scheduler)
+        CosineAnnealingWarmRestarts_test()
+    WarmupLR_test()
