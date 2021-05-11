cf55af45dc9c7ceec7a83de20791a76ae5aad6c0
Junghoon Kang <junghoon.jung.kang@gmail.com>
2021-05-11 09:53:18 +0900

    Update - model/criterion.py - added temperature_scale parameter in CrossEntropyLoss() and CrossEntropyLoss2d() classes

--- model/criterion.py
+++ model/criterion.py
@@ -8,15 +8,20 @@ __all__ = [
 
 
 class CrossEntropyLoss(nn.CrossEntropyLoss):
-    def __init__(self, weight=None):
+    def __init__(self, weight=None, temperature_scale=1.):
         super(CrossEntropyLoss, self).__init__(weight=weight, reduction='sum')
+        self.t_scale = temerature_scale
+
+    def forward(self, pred, true):
+        return super(CrossEntropyLoss, self).forward(pred / self.t_scale, true)
 
 class CrossEntropyLoss2d(nn.CrossEntropyLoss):
-    def __init__(self, weight=None):
+    def __init__(self, weight=None, temperature_scale=1.):
         super(CrossEntropyLoss2d, self).__init__(weight=weight, reduction='sum')
+        self.t_scale = temerature_scale
 
-    def forward(self, input, target):
-        N, C, H, W = input.size()
-        pred = input.transpose(1,2).transpose(2,3).contiguous().view(-1, C)
-        true = target.transpose(1,2).transpose(2,3).long().contiguous().flatten()
-        return super(CrossEntropyLoss2d, self).forward(pred, true)
+    def forward(self, pred, true):
+        N, C, H, W = pred.size()
+        pred = pred.transpose(1,2).transpose(2,3).contiguous().view(-1, C)
+        true = true.transpose(1,2).transpose(2,3).long().contiguous().flatten()
+        return super(CrossEntropyLoss2d, self).forward(pred / self.t_scale, true)
