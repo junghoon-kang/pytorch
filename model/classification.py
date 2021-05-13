ebf69e1872000f75e1d2cc42061f5985c4a61ad5
Junghoon Kang <junghoon.jung.kang@gmail.com>
2021-05-13 09:46:57 +0900

    Add - model/classification.py - added Classification() module

---
+++ model/classification.py
@@ -0,0 +1,65 @@
+import torch.nn.functional as F
+from torchmetrics import MetricCollection
+from pytorch_lightning import LightningModule
+
+
+__all__ = [
+    "Classification"
+]
+
+
+class Classification(LightningModule):
+    def __init__(self, network, criterion, optimizer, scheduler=None, regularizer=None, metrics=[], **kwargs):
+        super().__init__()
+
+        self.network = network
+        self.criterion = criterion
+        self.optimizer = optimizer
+        self.scheduler = scheduler
+        self.regularizer = regularizer
+        self.metrics = MetricCollection(metrics)
+
+    def forward(self, x):
+        return self.network(x)
+
+    def training_step(self, batch, batch_idx):
+        x, y, _ = batch
+        y_ = self(x)
+        loss = self.criterion(y_, y)
+        if self.regularizer is not None:
+            loss += self.regularizer.calculate_loss()
+        self.log("train_loss", loss, on_step=False, on_epoch=True, logger=True)
+        return loss
+
+    def validation_step(self, batch, batch_idx):
+        x, y, _ = batch
+        y_ = self(x)
+        loss = self.criterion(y_, y)
+        prob = F.softmax(y_, dim=1)
+        self.metrics.update(prob, y)
+        self.log("val_loss", loss, on_epoch=True, logger=True)
+
+    def validation_epoch_end(self, outputs):
+        results = self.metrics.compute()
+        for k, v in sorted(results.items()):
+            self.log(f"val_{k}", v, on_epoch=True, logger=True)
+        self.metrics.reset()
+
+    def test_step(self, batch, batch_idx):
+        x, y, _ = batch
+        y_ = self(x)
+        loss = self.criterion(y_, y)
+        prob = F.softmax(y, dim=1)
+        self.metrics.update(prob, y)
+
+    def test_epoch_end(self, outputs):
+        results = self.metrics.compute()
+        for k, v in sorted(results.items()):
+            self.log(k, v, logger=True)
+        self.metrics.reset()
+
+    def configure_optimizers(self):
+        if self.scheduler is None:
+            return self.optimizer
+        else:
+            return [self.optimizer], [self.scheduler]
