6c204018bbd3d94d0f9efaacadf8ba20ac3f2c1c
Junghoon Kang <junghoon.kang@cognex.com>
2021-05-26 13:22:12 +0900

    Update - tests/unit/test_dataset.py - renamed setup_dataset_path() fixture function to dataset_path()

--- tests/unit/test_dataset.py
+++ tests/unit/test_dataset.py
@@ -6,14 +6,13 @@ from albumentations.pytorch import ToTensorV2 as ToTensor
 
 import os, sys
 PATH = os.path.dirname(os.path.abspath(__file__))
-sys.path.insert(0, os.path.join(PATH, "..", ".."))
+sys.path.insert(0, os.path.join(PATH, *[".."]*2))
 import config
 from vision.dataset import *
-from vision.transform import *
 
 
 @pytest.fixture
-def setup_dataset_path():
+def dataset_path():
     path = os.path.join(config.DATA_DIR, "public", "DAGM", "original")
     image_dirpath = os.path.join(path, "image")
     annotation_filepath = os.path.join(path, "annotation", "domain1.single_image.2class.json")
@@ -21,8 +20,8 @@ def setup_dataset_path():
     seg_label_dirpath = os.path.join(path, "mask", "original.2class")
     return [image_dirpath, annotation_filepath, imageset_filepath, seg_label_dirpath]
 
-def test_SingleImageClassificationDataset_1(setup_dataset_path):
-    image_dirpath, annotation_filepath, imageset_filepath, seg_label_dirpath = setup_dataset_path
+def test_SingleImageClassificationDataset_1(dataset_path):
+    image_dirpath, annotation_filepath, imageset_filepath, seg_label_dirpath = dataset_path
     dataset = SingleImageClassificationDataset(
         image_dirpath,
         annotation_filepath,
@@ -36,8 +35,8 @@ def test_SingleImageClassificationDataset_1(setup_dataset_path):
     assert isinstance(y, int)
     assert isinstance(name, str)
 
-def test_SingleImageClassificationDataset_2(setup_dataset_path):
-    image_dirpath, annotation_filepath, imageset_filepath, seg_label_dirpath = setup_dataset_path
+def test_SingleImageClassificationDataset_2(dataset_path):
+    image_dirpath, annotation_filepath, imageset_filepath, seg_label_dirpath = dataset_path
     dataset = SingleImageClassificationDataset(
         image_dirpath,
         annotation_filepath,
@@ -53,76 +52,17 @@ def test_SingleImageClassificationDataset_2(setup_dataset_path):
     )
     for x, y, name in data_loader:
         assert isinstance(x, torch.Tensor)
+        assert x.size() == torch.Size([1,1,512,512])
         assert isinstance(y, torch.Tensor)
-        assert isinstance(name, tuple)
+        assert y.size() == torch.Size([1])
+        assert isinstance(name, list)
+        assert len(name) == 1
         assert isinstance(name[0], str)
         break
 
 @pytest.mark.parametrize("batch_size", [4,8])
-def test_SingleImageClassificationDataset_3(setup_dataset_path, batch_size):
-    image_dirpath, annotation_filepath, imageset_filepath, seg_label_dirpath = setup_dataset_path
-    dataset = SingleImageClassificationDataset(
-        image_dirpath,
-        annotation_filepath,
-        imageset_filepath,
-        transforms=[ToTensor()]
-    )
-    data_loader = DataLoader(
-        dataset,
-        batch_size=batch_size,
-        shuffle=False,
-        num_workers=1,
-        pin_memory=True
-    )
-    for x, y, name in data_loader:
-        assert x.size() == torch.Size([batch_size,1,512,512])
-        assert y.size() == torch.Size([batch_size])
-        assert len(name) == batch_size
-        break
-
-def test_SingleImageSegmentationDataset_1(setup_dataset_path):
-    image_dirpath, annotation_filepath, imageset_filepath, seg_label_dirpath = setup_dataset_path
-    dataset = SingleImageSegmentationDataset(
-        image_dirpath,
-        annotation_filepath,
-        imageset_filepath,
-        seg_label_dirpath,
-        transforms=[]
-    )
-    assert len(dataset) > 0
-    x, y, name = dataset[0]
-    assert isinstance(x, np.ndarray)
-    assert x.shape == (512,512)
-    assert isinstance(y, np.ndarray)
-    assert y.shape == (512,512)
-    assert isinstance(name, str)
-
-def test_SingleImageSegmentationDataset_2(setup_dataset_path):
-    image_dirpath, annotation_filepath, imageset_filepath, seg_label_dirpath = setup_dataset_path
-    dataset = SingleImageSegmentationDataset(
-        image_dirpath,
-        annotation_filepath,
-        imageset_filepath,
-        seg_label_dirpath,
-        transforms=[]
-    )
-    data_loader = DataLoader(
-        dataset,
-        batch_size=1,
-        shuffle=False,
-        num_workers=1,
-        pin_memory=True
-    )
-    for x, y, name in data_loader:
-        assert isinstance(x, torch.Tensor)
-        assert isinstance(y, torch.Tensor)
-        assert isinstance(name, tuple)
-        assert isinstance(name[0], str)
-        break
-
-@pytest.mark.parametrize("batch_size", [4,8])
-def test_SingleImageClassificationDataset_3(setup_dataset_path, batch_size):
-    image_dirpath, annotation_filepath, imageset_filepath, seg_label_dirpath = setup_dataset_path
+def test_SingleImageClassificationDataset_3(dataset_path, batch_size):
+    image_dirpath, annotation_filepath, imageset_filepath, seg_label_dirpath = dataset_path
     dataset = SingleImageClassificationDataset(
         image_dirpath,
         annotation_filepath,
@@ -142,8 +82,8 @@ def test_SingleImageClassificationDataset_3(setup_dataset_path, batch_size):
         assert len(name) == batch_size
         break
 
-def test_SingleImageSegmentationDataset_1(setup_dataset_path):
-    image_dirpath, annotation_filepath, imageset_filepath, seg_label_dirpath = setup_dataset_path
+def test_SingleImageSegmentationDataset_1(dataset_path):
+    image_dirpath, annotation_filepath, imageset_filepath, seg_label_dirpath = dataset_path
     dataset = SingleImageSegmentationDataset(
         image_dirpath,
         annotation_filepath,
@@ -159,8 +99,8 @@ def test_SingleImageSegmentationDataset_1(setup_dataset_path):
     assert y.shape == (512,512)
     assert isinstance(name, str)
 
-def test_SingleImageSegmentationDataset_2(setup_dataset_path):
-    image_dirpath, annotation_filepath, imageset_filepath, seg_label_dirpath = setup_dataset_path
+def test_SingleImageSegmentationDataset_2(dataset_path):
+    image_dirpath, annotation_filepath, imageset_filepath, seg_label_dirpath = dataset_path
     dataset = SingleImageSegmentationDataset(
         image_dirpath,
         annotation_filepath,
@@ -177,14 +117,17 @@ def test_SingleImageSegmentationDataset_2(setup_dataset_path):
     )
     for x, y, name in data_loader:
         assert isinstance(x, torch.Tensor)
+        assert x.size() == torch.Size([1,1,512,512])
         assert isinstance(y, torch.Tensor)
-        assert isinstance(name, tuple)
+        assert y.size() == torch.Size([1,512,512])
+        assert isinstance(name, list)
+        assert len(name) == 1
         assert isinstance(name[0], str)
         break
 
 @pytest.mark.parametrize("batch_size", [4,8])
-def test_SingleImageSegmentationDataset_3(setup_dataset_path, batch_size):
-    image_dirpath, annotation_filepath, imageset_filepath, seg_label_dirpath = setup_dataset_path
+def test_SingleImageSegmentationDataset_3(dataset_path, batch_size):
+    image_dirpath, annotation_filepath, imageset_filepath, seg_label_dirpath = dataset_path
     dataset = SingleImageSegmentationDataset(
         image_dirpath,
         annotation_filepath,
