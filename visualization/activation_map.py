b3129c76eaa2e96883f5cacdcc96f68c61aefe5e
Junghoon Kang <junghoon.jung.kang@gmail.com>
2021-05-31 17:29:53 +0900

    Update - visualization/activation_map.py - ActivationMap() module now inherits LightningModule()

--- visualization/activation_map.py
+++ visualization/activation_map.py
@@ -1,30 +1,27 @@
 import os
-import cv2
-import shutil
-import skimage.io
-import skimage.transform
 import numpy as np
-import torch
-import torch.utils.data
-import torch.autograd
-from abc import ABCMeta, abstractmethod, abstractproperty
+from pytorch_lightning import LightningModule
 
 
-__all__ = ['ActivationMap']
+__all__ = ["ActivationMap"]
 
 
-class ActivationMap:
-    def __init__(self, model, activation_layers, args):
-        self.model = model
-        self.activation_layers = activation_layers
-        self.gpu = args.gpu
-        self.batch_size = args.batch_size
-        self.num_workers = args.num_workers
+class ActivationMap(LightningModule):
+    def __init__(self, network, layer_names=[], activation_checkpoint_dir="activation_checkpoint"):
+        super().__init__()
+        self.network = network
+        self.layer_names = layer_names
         self.register_hook()
+        self.automatic_optimization = False
+
+        for dirname in layer_names:
+            if not os.path.exists(os.path.join(activation_checkpoint_dir, dirname)):
+                os.makedirs(os.path.join(activation_checkpoint_dir, dirname))
+        self.activation_checkpoint_dir = activation_checkpoint_dir
 
     def register_hook(self):
-        for name, layer in self.model.named_modules():
-            if name in self.activation_layers:
+        for name, layer in self.network.named_modules():
+            if name in self.layer_names:
                 layer.register_forward_hook(self.append_activation(name))
 
     def append_activation(self, name):
@@ -32,29 +29,25 @@ class ActivationMap:
             self.activations[name].append(y)
         return _append_activation
 
-    def setup(self):
-        self.activations = { layer: [] for layer in self.activation_layers }
+    def forward(self, x):
+        return self.network(x)
 
-    def checkpoint(self, dataset, activation_checkpoint_dir='activation_checkpoint'):
-        for activation_layer in self.activation_layers:
-            if not os.path.exists(f'{activation_checkpoint_dir}/{activation_layer}'):
-                os.makedirs(f'{activation_checkpoint_dir}/{activation_layer}')
+    def training_step(self, batch, batch_idx):
+        self.clear()
+        self.eval()
+        x, y, name = batch  # x: 16x3x120x200, y: 16
+        y_ = self(x)  # y_: 16x2
+        for layer in self.layer_names:
+            for i in range(len(name)):
+                activation = self.activations[layer][0][i]
+                image_name = ".".join(name[i].split(".")[:-1]) + ".npy"
+                np.save(
+                    os.path.join(self.activation_checkpoint_dir, layer, image_name),
+                    activation.cpu().detach().numpy()
+                )
 
-        data_loader = torch.utils.data.DataLoader(
-            dataset,
-            batch_size=self.batch_size,
-            shuffle=False,
-            num_workers=self.num_workers,
-            pin_memory=True
-        )
+    def clear(self):
+        self.activations = { layer: [] for layer in self.layer_names }
 
-        self.model.eval()
-        for x, y, name in data_loader:
-            self.setup()
-            x = x.cuda(self.gpu)  # x: 16x3x512x512
-            y_ = self.model(x)  # y_: 16x2
-            for layer in self.activation_layers:
-                for i in range(len(name)):
-                    activation = self.activations[layer][0][i]
-                    image_name = '.'.join(name[i].split('.')[:-1])
-                    np.save(f'{activation_checkpoint_dir}/{layer}/{image_name}.npy', activation.cpu().detach().numpy())
+    def configure_optimizers(self):
+        return None
