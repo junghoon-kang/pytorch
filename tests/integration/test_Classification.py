01ea6e6c4a353c8eb00e536d94e92b8822f127c1
Junghoon Kang <junghoon.jung.kang@gmail.com>
2021-06-16 00:03:26 +0900

    Update - tests/integration/test_Classification.py

--- tests/integration/test_Classification.py
+++ tests/integration/test_Classification.py
@@ -7,7 +7,8 @@ import albumentations as A
 import torchmetrics
 from pytorch_lightning import Trainer
 from pytorch_lightning.loggers import TensorBoardLogger
-from pytorch_lightning.callbacks import ModelCheckpoint, LearningRateMonitor
+#from pytorch_lightning.callbacks import ModelCheckpoint, LearningRateMonitor
+from pytorch_lightning.callbacks import LearningRateMonitor
 
 
 PATH = os.path.dirname(os.path.abspath(__file__))
@@ -21,6 +22,7 @@ from model.classification import *
 from model.network.classification.resnet import *
 from model.criterion import *
 from model.optimizer import *
+from model.checkpointer import *
 
 
 @pytest.fixture
@@ -158,3 +160,108 @@ def test_evaluate(model, dataloaders):
     assert result[0]["test_Recall"] > 0.99, result
     assert trainer.logged_metrics['train_loss'] < 0.1, trainer.logged_metrics
     assert trainer.logged_metrics['valid_loss'] < 0.1, trainer.logged_metrics
+
+
+if __name__ == "__main__":
+    path = os.path.join(config.DATA_DIR, "public", "DAGM", "original.rescaled256")
+    image_dirpath = os.path.join(path, "image")
+    annotation_filepath = os.path.join(path, "annotation", "domain1.single_image.2class.json")
+    imageset_dirpath = os.path.join(path, "imageset", "domain1.single_image.2class", "public", "ratio", "100%")
+    seg_label_dirpath = os.path.join(path, "mask", "original.2class")
+    train_filepath = os.path.join(imageset_dirpath, "train.1.txt")
+    valid_filepath = os.path.join(imageset_dirpath, "validation.1.txt")
+    test_filepath  = os.path.join(imageset_dirpath, "test.txt")
+
+    train_annotation = SingleImageAnnotation(num_classes=2)
+    valid_annotation = SingleImageAnnotation(num_classes=2)
+    test_annotation = SingleImageAnnotation(num_classes=2)
+    train_annotation.from_research_format(image_dirpath, annotation_filepath, train_filepath, seg_label_dirpath)
+    valid_annotation.from_research_format(image_dirpath, annotation_filepath, valid_filepath, seg_label_dirpath)
+    test_annotation.from_research_format(image_dirpath, annotation_filepath, test_filepath, seg_label_dirpath)
+
+    train_dataset = ClassificationDataset(
+        train_annotation,
+        transforms=[
+            A.HorizontalFlip(p=.5),
+            A.VerticalFlip(p=.5),
+            To3channel(),
+            ToTensor(),
+        ]
+    )
+    valid_dataset = ClassificationDataset(
+        valid_annotation,
+        transforms=[
+            To3channel(),
+            ToTensor(),
+        ]
+    )
+    test_dataset = ClassificationDataset(
+        test_annotation,
+        transforms=[
+            To3channel(),
+            ToTensor(),
+        ]
+    )
+
+    sampler = WeightedSampler([1,1])
+
+    train_dataloader = DataLoader(
+        train_dataset,
+        batch_size=32,
+        sampler=sampler(train_dataset),
+        shuffle=False,
+        num_workers=4,
+        pin_memory=True
+    )
+
+    valid_dataloader = DataLoader(
+        valid_dataset,
+        batch_size=32,
+        shuffle=False,
+        num_workers=4,
+        pin_memory=True
+    )
+
+    test_dataloader = DataLoader(
+        test_dataset,
+        batch_size=32,
+        shuffle=False,
+        num_workers=4,
+        pin_memory=True
+    )
+
+    network = ResNet18(num_classes=2)
+    criterion = CrossEntropyLoss()
+    optimizer = Adam(network.parameters(), lr=0.0001, weight_decay=0.0001)
+
+    model = Classification(
+        network, criterion, optimizer,
+        metrics=[
+            torchmetrics.Accuracy(),
+            torchmetrics.Recall(average="macro", num_classes=2),
+        ]
+    )
+
+    logger = TensorBoardLogger(save_dir=os.path.join(PATH, "checkpoint"), name="main")
+
+    checkpoint_callback = ModelCheckpoint(
+        save_top_k=5,
+        save_last=False,
+        monitor="valid_Recall",
+        mode="max",
+        dirpath=os.path.join(logger.root_dir, f"version_{logger.version}"),
+        filename="{epoch:03d}-{valid_Recall:.4f}",
+    )
+
+    trainer = Trainer(
+        logger=logger,
+        callbacks=[
+            LearningRateMonitor(),
+            checkpoint_callback,
+        ],
+        gpus=1,
+        max_epochs=300,
+        min_epochs=1
+    )
+    trainer.fit(model, train_dataloader, valid_dataloader)
+    print(trainer.logged_metrics)
