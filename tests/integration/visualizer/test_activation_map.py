5794d54153d2f66e891ec49305930e7c494f6248
Junghoon Kang <junghoon.kang@cognex.com>
2021-07-13 14:15:42 +0900

    Update - tests/integration/visualizer/*.py - cascade updates from 'bf59964'

--- tests/integration/visualizer/test_activation_map.py
+++ tests/integration/visualizer/test_activation_map.py
@@ -1,94 +1,38 @@
 import os, sys
-import glob
 import pytest
 import torch
 from torch.utils.data import DataLoader
 from pytorch_lightning import Trainer
 
-
 PATH = os.path.dirname(os.path.abspath(__file__))
-sys.path.insert(0, os.path.join(PATH, *[".."]*2))
-import config
-from vision.dataset import *
-from vision.transform import *
-from vision.sampler import *
-from model.network.classification.resnet import *
-from visualization.activation_map import *
+sys.path.insert(0, os.path.join(PATH, *[".."]*3))
+from network.classification.resnet import ResNet18
+from visualizer.activation_map import ActivationMap
+from tests.integration.visualizer.fixture import *
 
 
-path = os.path.join(config.DATA_DIR, "public", "DAGM", "original.rescaled256")
-image_dirpath = os.path.join(path, "image")
-annotation_filepath = os.path.join(path, "annotation", "domain1.single_image.2class.json")
-imageset_dirpath = os.path.join(path, "imageset", "domain1.single_image.2class", "public", "ratio", "100%")
-seg_label_dirpath = os.path.join(path, "mask", "original.2class")
-train_filepath = os.path.join(imageset_dirpath, "train.1.txt")
-valid_filepath = os.path.join(imageset_dirpath, "validation.1.txt")
-test_filepath  = os.path.join(imageset_dirpath, "test.txt")
+def test_ActivationMap(dataloaders, ckpt_path):
+    train_dataloader, valid_dataloader, test_dataloader = dataloaders
 
-train_dataset = SingleImageClassificationDataset(
-    image_dirpath, annotation_filepath, train_filepath, seg_label_dirpath,
-    transforms=[
-        To3channel(),
-        ToTensor(),
-    ]
-)
-valid_dataset = SingleImageClassificationDataset(
-    image_dirpath, annotation_filepath, valid_filepath, seg_label_dirpath,
-    transforms=[
-        To3channel(),
-        ToTensor(),
-    ]
-)
-test_dataset = SingleImageClassificationDataset(
-    image_dirpath, annotation_filepath, test_filepath, seg_label_dirpath,
-    transforms=[
-        To3channel(),
-        ToTensor(),
-    ]
-)
+    network = ResNet18(num_classes=2)
+    model = ActivationMap(
+        network,
+        layer_names=[
+            "layer1.1.bn2",
+            "layer2.1.bn2",
+            "layer3.1.bn2",
+            "layer4.1.bn2",
+        ],
+        checkpoint_dir=os.path.join("image_checkpoint", "activation_map")
+    )
 
-train_dataloader = DataLoader(
-    train_dataset,
-    batch_size=32,
-    shuffle=False,
-    num_workers=4,
-    pin_memory=True
-)
+    ckpt = torch.load(ckpt_path)
+    model.load_state_dict(ckpt["state_dict"])
 
-valid_dataloader = DataLoader(
-    valid_dataset,
-    batch_size=32,
-    shuffle=False,
-    num_workers=4,
-    pin_memory=True
-)
-
-test_dataloader = DataLoader(
-    test_dataset,
-    batch_size=32,
-    shuffle=False,
-    num_workers=1,
-    pin_memory=True
-)
-
-network = ResNet18(num_classes=2)
-model = ActivationMap(
-    network,
-    layer_names=[
-        "layer1.1.bn2",
-        "layer2.1.bn2",
-        "layer3.1.bn2",
-        "layer4.1.bn2",
-    ]
-)
-
-ckpt = torch.load(os.path.join(PATH, "checkpoint", "main", "version_0", "last.ckpt"))
-model.load_state_dict(ckpt["state_dict"])
-
-trainer = Trainer(
-    logger=False,
-    checkpoint_callback=False,
-    gpus=1,
-    max_epochs=1
-)
-trainer.fit(model, test_dataloader)
+    trainer = Trainer(
+        logger=False,
+        checkpoint_callback=False,
+        gpus=1,
+        max_epochs=1
+    )
+    trainer.fit(model, test_dataloader)
